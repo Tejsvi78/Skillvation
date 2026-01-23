071f5340812fb5d36494a18bae419bb5bb2ff5b0
Tejsvi78 <tejsvisharma2@gmail.com>
2026-01-23 18:20:35 +0530

    connectionDB function added

---
+++ config/connectDB.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+exports.connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error.message);
+        process.exit(1);
+    }
+}
