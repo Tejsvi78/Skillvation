f47365cbda85b898beb6a45c94ca0d09676d3632
Tejsvi Sharma <tejsvisharma2@gmail.com>
2026-03-15 20:27:14 +0530

    signature field added

--- models/Payment.js
+++ models/Payment.js
@@ -1,51 +1,55 @@
 const mongoose = require("mongoose");
 
-const paymentSchema = new mongoose.Schema({
+const paymentSchema = new mongoose.Schema(
+  {
     student: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-        index: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
     },
 
     course: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Course",
-        required: true,
-        index: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+      index: true,
     },
 
     razorpayOrderId: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
 
     razorpayPaymentId: {
-        type: String,
+      type: String,
     },
-
+    razorpaySignature: {
+      type: String,
+    },
     amount: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
 
     platformFee: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
 
     instructorEarning: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
 
     status: {
-        type: String,
-        enum: ["Pending", "Success", "Failed"],
-        default: "Pending",
+      type: String,
+      enum: ["Pending", "Success", "Failed"],
+      default: "Pending",
     },
-
-}, { timestamps: true });
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("Payment", paymentSchema);
